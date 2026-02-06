000578b7d5ac482ee2867ab3517ccb049af3b5fa
udayhasan <udayhasan15@gmail.com>
2026-02-06 23:19:55 +0600

    changes

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_AUTH_URL!,
-  // baseURL: "https://check-mocha-three.vercel.app",
+  // baseURL: process.env.NEXT_PUBLIC_AUTH_URL!,
+  baseURL: "https://check-front-pi.vercel.app",
   plugins: [
     inferAdditionalFields({
       user: { role: { type: "string" } },
